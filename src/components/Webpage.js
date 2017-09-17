1e47eb415e7859a03008f888c3192407556af34a
J Fernandez <jffernan@comcast.net>
2017-09-17 21:35:34 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import FillerText from './FillerText';
 
-export default class FillerText extends React.Component {
+export default class Webpage extends React.Component {
   render() {
     return (
       <div>
-      I am a filler text. I can be used to fill your screen. Amazing!
+        <h1>The world's coolest webpage</h1>
+        <FillerText />
+        <FillerText />
       </div>
     );
   }
