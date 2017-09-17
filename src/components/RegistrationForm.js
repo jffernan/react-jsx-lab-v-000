ad2e29cbccae1088b78dfba02ffe887532ed8f46
J Fernandez <jffernan@comcast.net>
2017-09-17 21:28:02 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -3,12 +3,11 @@ import React from 'react';
 class RegistrationForm extends React.Component {
   render() {
     return (
-      <div className="registration_form">
-        <img src="http://twitter.com/some-avatar.png" className="tweet__avatar" />
-        <div className="tweet__body">
-          <p>We are writing this tweet in JSX. Holy moly!</p>
-        </div>
-      </div>
+      <form>
+      <label>Username</label>
+      <input type="text" />
+      
+      </form>
     );
   }
 }
