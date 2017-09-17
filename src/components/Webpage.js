e2090743b9d2c4a016a8a8513f0a9266bd1472f5
J Fernandez <jffernan@comcast.net>
2017-09-17 21:33:35 +0000

    automatically backed up by learn

---
+++ src/components/Webpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import FillerText from './FillerText';
+
+export default class FillerText extends React.Component {
+  render() {
+    return (
+      <div>
+      I am a filler text. I can be used to fill your screen. Amazing!
+      </div>
+    );
+  }
+}
