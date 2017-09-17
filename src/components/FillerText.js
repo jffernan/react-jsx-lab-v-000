90ee83eeba280125eae9b08c06d99342f8956fd8
J Fernandez <jffernan@comcast.net>
2017-09-17 21:31:34 +0000

    automatically backed up by learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -3,13 +3,9 @@ import React from 'react';
 export default class FillerText extends React.Component {
   render() {
     return (
-      <form>
-      <label>Username</label>
-      <input type="text" />
-      <label>Password</label>
-      <input type="password" />
-      <button type="submit">Register</button>
-      </form>
+      <p>
+      I am a filler text. I can be used to fill your screen. Amazing!
+      </p>
     );
   }
 }
